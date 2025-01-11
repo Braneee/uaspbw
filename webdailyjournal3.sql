0ff1025e46ba22d076531c625b23cff4c435efe4
Gibran <raishilmy0@gmail.com>
2025-01-11 20:40:26 +0700

    first commit

--- webdailyjournal3.sql
+++ webdailyjournal3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2025 at 01:06 PM
+-- Generation Time: Jan 11, 2025 at 02:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -41,9 +41,7 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
-(6, 'Acer Luncurkan Rangkaian Laptop Gaming Nitro V Terbaru dengan Dukungan AI', 'Acer baru saja mengumumkan rangkaian laptop gaming Nitro V terbaru dengan berbagai ukuran layar, dirancang untuk memenuhi segmen laptop gaming entry level dengan spesifikasi yang mendukung berbagai komputasi AI. Nitro V AI terbaru hadir dengan sejumlah model pilihan, termasuk Nitro V 17 AI, Nitro V 16 AI, dan Nitro V 14 AI.', '20250111184935.jpg', '2025-01-11 18:49:35', 'admin'),
-(7, 'Rumor iPhone 17 Series akan Hadir dengan Inovasi Menarik di 2025', 'Meskipun baru tiga bulan sejak peluncuran iPhone 16 series pada bulan September, rumor mengenai iPhone 17 series yang akan datang sudah mulai beredar. Menurut laporan, Apple akan melakukan perubahan besar pada tahun 2025 dengan memperkenalkan model baru yang menggantikan varian Plus yang sudah lama ada, kemungkinan besar akan disebut iPhone 17 Air atau Slim. Ini berarti kita bisa melihat Apple meluncurkan empat model tahun depan: iPhone 17, iPhone 17 Air, iPhone 17 Pro, dan iPhone 17 Pro Max. Perangkat-perangkat ini dikabarkan akan memiliki kemampuan AI canggih, desain baru, dan peningkatan perangkat keras yang signifikan.', '20250111185030.jpg', '2025-01-11 18:50:30', 'admin'),
-(8, 'CES 2025: GIGABYTE Hadirkan GeForce RTX 50 Series dengan Teknologi Pendinginan Mutakhir', 'GIGABYTE baru saja mengumumkan peluncuran kartu grafis NVIDIA GeForce RTX 50 Series yang ditenagai oleh NVIDIA Blackwell dan AI di gelaran CES 2025. Seri terbaru ini mencakup GeForce RTX 5090, RTX 5090 D, RTX 5080, RTX 5070 Ti, dan RTX 5070.', '20250111185057.jpeg', '2025-01-11 18:50:57', 'admin');
+(6, 'Aku senang', 'Aku sedih', '20250111200551.jpg', '2025-01-11 20:05:51', 'admin');
 
 -- --------------------------------------------------------
 
@@ -61,10 +59,7 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`id`, `image_url`) VALUES
-(1, '20250111185115.jpg'),
-(2, '20250111185123.jpeg'),
-(3, '20250111185130.jpg'),
-(4, '20250111185137.jpg');
+(2, '20250111200633.jpg');
 
 -- --------------------------------------------------------
 
@@ -85,8 +80,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`, `role`) VALUES
-(1, 'admin', 'd41d8cd98f00b204e9800998ecf8427e', '20250111184409.jpg', 'admin'),
-(2, 'gibran', 'd41d8cd98f00b204e9800998ecf8427e', '20250111184346.jpg', 'user');
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '20250111200358.jpg', 'admin'),
+(3, 'gibran', '23f62dd0358adb25f5666ba9b1f6fe44', '20250111200500.jpg', 'admin'),
+(4, 'danny', '21232f297a57a5a743894a0e4a801fc3', '20250111201402.png', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -118,19 +114,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
